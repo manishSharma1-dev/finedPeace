b12cfda81c254f772cba86978f00f262ed172795
manish sharma dev <manishvsharma1@gmail.com>
2024-10-17 23:42:41 -1200

    Added Caching in getallThought route

--- app/api/(thought)/getallThought/route.ts
+++ app/api/(thought)/getallThought/route.ts
@@ -2,6 +2,9 @@ import { thoughtModel } from "@/model/thoughts.model";
 import { ConnectDb } from "@/connections/dbConnect";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
+import NodeCache from "node-cache"
+
+const myCache = new NodeCache();
 
 export async function GET(request:Request) {
     await ConnectDb()
@@ -22,42 +25,61 @@ export async function GET(request:Request) {
             )
         }
 
-        const response = await thoughtModel.find({ }).sort({ createdAt : -1 })
+        const cacheKey = JSON.stringify("Alldata")
+        const CachedResult = myCache.get(cacheKey)
 
-        if(!response){
+        if(CachedResult){
+            console.log("Data already Present in Cache")
+
             return Response.json(
                 {
-                    success: false,
-                    message: "Thoughts fetching Failed"
-                },
-                {
-                    status : 500
+                    success : true,
+                    message : "data already in cache",
+                    data : CachedResult
                 }
             )
-        }
 
-        if(response.length < 0){
+        } else {
+
+            const response = await thoughtModel.find({ }).sort({ createdAt : -1 })
+
+            if(!response){
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Thoughts fetching Failed"
+                    },
+                    {
+                        status : 500
+                    }
+                )
+            }
+
+            if(response.length < 0){
+                return Response.json(
+                    {
+                        success  : true,
+                        message: "No thoughts avialable"
+                    },
+                    {
+                        status : 200
+                    }
+                )
+            }
+
+            myCache.set(cacheKey,response,3600)
+
             return Response.json(
                 {
                     success  : true,
-                    message: "No thoughts avialable"
+                    message: "No thoughts avialable",
+                    data  : response
                 },
                 {
                     status : 200
                 }
-            )
-        }
-
-        return Response.json(
-            {
-                success  : true,
-                message: "No thoughts avialable",
-                data  : response
-            },
-            {
-                status : 200
-            }
-        )    
+            ) 
+        }   
         
     } catch (error) {
         console.error("Fetching all thougts failed",error)
