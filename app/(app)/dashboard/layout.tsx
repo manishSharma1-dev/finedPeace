a87afee94a451f02ac3862df6863f38fa8f49a88
manish sharma dev <manishvsharma1@gmail.com>
2025-02-07 01:51:18 -1200

    made dashboard responsive for bigger screen

--- app/(app)/dashboard/layout.tsx
+++ app/(app)/dashboard/layout.tsx
@@ -11,28 +11,33 @@ interface Dashboardlayoutprops {
 const DashboardLayout: React.FC<Dashboardlayoutprops> = (props) => {
   return (
      <AuthProvider>
-      <div className="bg-black text-white overflow-hidden">
-        <div className="grid grid-cols-7">
-        <div className="col-start-1 col-end-2 min-h-screen border-r">
-          <div className="flex-col flex justify-between min-h-screen p-3">
-              <div className='flex flex-col text-xs gap-3'>
-                  <p className='cursor-pointer hover:opacity-60'>
-                    <Link href={'/dashboard/thoughts'}>Thoughts</Link>
-                  </p>
-                  
-                  <p className='cursor-pointer hover:opacity-60'>
-                    <Link href={'/dashboard/profile'}>Profile</Link>
-                  </p>
-              </div>
-              <div className='text-xs'>
-                  <button className='hover:bg-gray-300 p-1 pl-3 pr-3 border rounded hover:text-black' onClick={() => signOut({ callbackUrl: '/sign-in' })}>Logout</button>
-              </div>
+      <div className="bg-black text-white overflow-hidden xl:px-28 2xl:px-48 3xl:px-56 4xl:pl-[18rem] 4xl:pr-24 6xl:pl-[28rem] 6xl:pr-32 7xl:pl-[32rem] 7xl:pr-52">
+
+        <div className="grid xs:grid-cols-1 sm:grid-cols-1 lg:grid-cols-7">
+
+          <div className="min-h-screen border-r xs:hidden sm:hidden lg:block lg:col-start-1 lg:col-end-2">
+            <div className="flex-col flex justify-between min-h-screen p-3">
+                <div className='flex flex-col text-xs gap-3 4xl:gap-6 6xl:gap-9 7xl:gap-14'>
+                    <p className='cursor-pointer hover:opacity-60 xl:text-sm 2xl:text-lg 4xl:text-2xl 6xl:text-4xl 7xl:text-5xl'>
+                      <Link href={'/dashboard/thoughts'}>Thoughts</Link>
+                    </p>
+                    
+                    <p className='cursor-pointer hover:opacity-60 xl:text-sm 2xl:text-lg 4xl:text-2xl 6xl:text-4xl 7xl:text-5xl'>
+                      <Link href={'/dashboard/profile'}>Profile</Link>
+                    </p>
+                </div>
+                <div className='text-xs xl:text-xm 2xl:text-base 4xl:text-xl 4xl:px-8 4xl:py-4 6xl:py-6 6xl:px-10 7xl:px-14 7xl:py-8'>
+                    <button className='hover:bg-gray-300 p-1 pl-3 pr-3 border rounded-lg hover:text-black  4xl:px-6 4xl:py-2 6xl:py-4 6xl:px-10 6xl:text-2xl 7xl:px-14 7xl:py-6 7xl:text-3xl' onClick={() => signOut({ callbackUrl: '/sign-in' })}>Logout</button>
+                </div>
+            </div>
           </div>
+
+          <div className="min-h-screen xs:col-start-1 xs:col-end-2 sm:col-start-1 sm:col-end-2 lg:col-start-2 lg:col-end-8">
+              {props.children}
+          </div>
+
         </div>
-        <div className="col-start-2 col-end-8 min-h-screen">
-            {props.children}
-        </div>
-        </div>
+
       </div>
     </AuthProvider>
   )
--- app/(app)/dashboard/thoughts/page.tsx
+++ app/(app)/dashboard/thoughts/page.tsx
@@ -10,6 +10,7 @@ import { useToast } from '@/hooks/use-toast'
 import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
 import { error } from '@/types/types'
+import { LoaderIcon } from 'lucide-react'
 
 type thoughtType = {
   _id : string;
@@ -100,7 +101,7 @@ export default function Page() {
       } 
     }
 
-     fetchallthoughtfrombackend();
+    //  fetchallthoughtfrombackend();
   },[newThoughtCreated,checkThoughtDeleted,toast])
 
 
@@ -141,93 +142,104 @@ export default function Page() {
   }
 
   return (
-   <div className='grid grid-cols-6'>
+   <div className='grid xs:grid-cols-1 sm:grid-cols-1 lg:grid-cols-6'>
 
-       <div className='col-start-1 col-end-5 min-h-screen border-r max-h-screen h-auto overflow-y-auto scrollbar-hide'>
+      <div className='min-h-screen border-r max-h-screen h-auto overflow-y-auto scrollbar-hide xs:col-start-1 xs:col-end-2 sm:col-start-1 sm:col-end-2  lg:col-start-1 lg:col-end-5'>
         {
           thoughtFetchedformBackend.length > 0 ? (
-            <div className='flex flex-col items-center gap-2 p-2 '>
+            // flex flex-col items-center gap-2 py-4
+            <div className='flex flex-col gap-2 py-2 xs:px-3 sm:px-4 lg:px-32 lg:items-center 3xl:gap-3 4xl:px-48 6xl:px-60 7xl:px-80 7xl:gap-6'>
               {
                 thoughtFetchedformBackend.map((thoughtField:thoughtType,idx:number) => (
-                  <div className='flex flex-col gap-2 text-sm w-[27rem] bg-neutral-800 pt-2 pb-2 pl-4 pr-4 rounded cursor-pointer' key={idx}>
+                  // flex flex-col gap-2 text-sm w-[27rem] bg-neutral-800 pt-2 pb-2 pl-4 px-8 rounded cursor-pointer
+                  <div className='flex flex-col gap-2 text-sm bg-neutral-800 pt-2 pb-2 pl-4 px-8 rounded hover:bg-neutral-700 xs:w-full sm:w-full lg:px-5 3xl:gap-5 6xl:gap-8 6xl:px-8 6xl:py-7 7xl:px-9 7xl:py-8 7xl:gap-12' key={idx}>
                     <div className='flex items-center justify-between pb-2'>
-                      <span>{`by - ${thoughtField?.username}`}</span>
-                      <XSquare size={14} className='hover:opacity-40' onClick={() => handledeltethoughtfromthebackend(thoughtField?._id)} />
+                      <span className='xs:text-xl sm:text-xl lg:text-sm 2xl:text-base 3xl:text-lg 4xl:text-2xl 6xl:text-[2.7rem] 6xl:leading-4 7xl:text-6xl'>{`by - ${thoughtField?.username}`}</span>
+                      <XSquare size={14} className='cursor-pointer hover:opacity-40 xs:w-4 xs:h-4 sm:w-5 sm:h-5 lg:w-4 lg:h-4 3xl:w-5 3xl:h-5 4xl:w-6 4xl:h-6 6xl:w-9 6xl:h-9 7xl:w-12 7xl:h-12' onClick={() => handledeltethoughtfromthebackend(thoughtField?._id)} />
                     </div>
-                    <p className='text-xs opacity-65 w-[80%]'>{thoughtField?.content}</p>
+                    <p className='text-xs opacity-65 w-[80%] xs:text-base sm:text-lg lg:text-xs 2xl:text-sm 3xl:text-base 4xl:text-xl 6xl:text-[2.1rem] 6xl:leading-[2.7rem] 7xl:text-[2.7rem] 7xl:leading-[3.5rem]'>{thoughtField?.content}</p>
                   </div>
                 ))
               } 
             </div>
           ) : ( 
-            <div className='flex justify-center items-center min-h-screen'>
-              <p className='text-sm opacity-70 hover:opacity-100 cursor-pointer animate-pulse'>Wait, fetching thoughts🎈 !</p>
+            <div className='flex justify-center items-center min-h-screen xs:px-4 sm:px-4 lg:px-0'>
+              <p className='text-sm opacity-70 hover:opacity-100 cursor-pointer animate-pulse xs:text-2xl sm:text-2xl lg:text-sm 2xl:text-base 4xl:text-lg 7xl:text-5xl'>Wait, fetching thoughts🎈 !</p>
             </div>
           )
         }
-       </div>
+      </div>
 
-       <div className='col-start-5 col-end-7 min-h-screen pl-7 pr-5'>
-          <div className='flex items-center gap-1 pt-3'>
-            <Asterisk size={13} color='red' />
-            <span className='text-xs'>Write here anything✍ !</span>
-          </div>
 
-          <div className='flex flex-col gap-1 pt-7 pl-5 text-xs flex-wrap'>
-            <p className='pl-3'>
-              <span>{'->'} </span>
-              <span className='opacity-40 italic animate-pulse'>anything that came to your mind...</span>
-            </p>
-            <p className='pl-3 '>
-              <span >{'->'} </span>
-              <span className='opacity-40'>drop your thought here...</span>
-            </p>
-            <p className='pl-3 '>
-              <span >{'->'} </span> 
-              <span className='opacity-40 italic animate-pulse'>things you cannot discuss with anyone...</span>
-            </p>
-            <p className=' pl-3 '>
-              <span >{'->'} </span> 
-              <span className='opacity-40'>Any Idea that came to your mind...</span>
-            </p>
-            <p className='pl-3 '>
-              <span >{'->'} </span> 
-              <span className='opacity-40 animate-pulse'>things you want to say but fear to say it...</span>
-            </p>
-          </div>
 
-          <div className='pt-16'>
-          <FormProvider {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-              <FormField
-                control={form.control}
-                name="thought"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className='flex items-center gap-2 pb-2'><span className='opacity-80 hover:opacity-100 cursor-pointer'>Your thought </span><MessageSquareQuote size={13} />{'!'}</FormLabel>
-                    <FormControl className='w-72'>
-                      <Textarea
-                        placeholder="What's in your mind ✍"
-                        className="resize-none text-xs overflow-y-auto scrollbar-hide" 
-                        {...field}
-                        rows={11}
-                      />
-                    </FormControl>
-                    <FormDescription>
-                      dump your thoughts here..
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <button type="submit" className='text-xs px-12 border py-2 rounded'>
-                {checkifthoughtAdded === true ? <div className='animate-bounce text-sm'><p>🐱‍🏍</p></div> : 'Submit'}
-              </button>
-            </form>
-          </FormProvider>
+      <div className='min-h-screen pl-7 pr-5 xs:hidden xs:invisible sm:hidden sm:invisible lg:block lg:visible lg:col-start-5 lg:col-end-7 xl:pt-2 3xl:pt-3 4xl:pl-9 4xl:pt-7 6xl:pl-12 6xl:pt-12 7xl:pt-16 7xl:pl-20'>
+
+        <div className='flex items-center gap-1 pt-3'>
+          <Asterisk size={13} color='red' className="xl:w-4 xl:h-4 2xl:w-5 2xl:h-5 4xl:w-8 4xl:h-8 6xl:w-12 6xl:h-12 7xl:w-16 7xl:h-16" />
+          <span className='text-xs lg:text-sm xl:text-base 2xl:text-lg 4xl:text-2xl 6xl:text-4xl 7xl:text-5xl'>Write here anything✍ !</span>
         </div>
 
-       </div>
+        <div className='flex flex-col gap-1 pt-7 pl-5 text-xs flex-wrap md:text-xs xl:text-xs 2xl:text-base 4xl:text-xl 6xl:text-4xl 6xl:pt-12 7xl:text-5xl 7xl:pt-16 7xl:leading-[3.4rem]'>
+          <p className='pl-3'>
+            <span>{'->'} </span>
+            <span className='opacity-40 italic animate-pulse'>anything that came to your mind...</span>
+          </p>
+          <p className='pl-3 '>
+            <span >{'->'} </span>
+            <span className='opacity-40'>drop your thought here...</span>
+          </p>
+          <p className='pl-3 '>
+            <span >{'->'} </span> 
+            <span className='opacity-40 italic animate-pulse'>things you cannot discuss with anyone...</span>
+          </p>
+          <p className=' pl-3 '>
+            <span >{'->'} </span> 
+            <span className='opacity-40'>Any Idea that came to your mind...</span>
+          </p>
+          <p className='pl-3 '>
+            <span >{'->'} </span> 
+            <span className='opacity-40 animate-pulse'>things you want to say but fear to say it...</span>
+          </p>
+        </div>
+
+        <div className='pt-16 xl:pt-12 2xl:pt-14 3xl:pt-20 4xl:pt-28 6xl:pt-32 7xl:pt-36'>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+            <FormField
+              control={form.control}
+              name="thought"  
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='flex items-center gap-2 pb-2 6xl:gap-3'>
+                    <span className='opacity-80 hover:opacity-100 cursor-pointer xl:text-base 2xl:text-lg 4xl:text-3xl 6xl:text-5xl 6xl:pb-5 7xl:text-6xl'>Your thought </span >
+                    <MessageSquareQuote size={13} className='3xl:w-6 3xl:h-6 6xl:w-9 6xl:h-9 7xl:w-12 7xl:h-12'/>
+                    <span className='3xl:text-lg 6xl:text-2xl 7xl:text-4xl'>{'!'} </span>
+                    </FormLabel>
+                  <FormControl className='w-72 3xl:w-[28rem] 4xl:w-[28rem] 6xl:w-[40rem] 7xl:w-[50rem]'>
+                    <Textarea
+                      placeholder="What's in your mind ✍"
+                      className="resize-none text-xs overflow-y-auto scrollbar-hide xl:text-sm 2xl:text-base 3xl:text-lg 4xl:text-2xl 4xl:h-[30vh] 6xl:text-4xl 6xl:h-[34vh] 7xl:text-5xl" 
+                      {...field}
+                      rows={11}
+                    />
+                  </FormControl>
+                  <FormDescription className='xl:text-sm 3xl:text-base 4xl:text-xl 6xl:text-3xl 7xl:text-4xl'>
+                    dump your thoughts here..
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <button type="submit" className='text-xs px-12 border py-2 rounded flex items-center justify-center xl:text-sm 4xl:text-lg 6xl:text-3xl 6xl:px-16 6xl:py-6 7xl:text-5xl'>
+              {checkifthoughtAdded === true ? (
+                    <LoaderIcon size={18} stroke="white" strokeWidth={2.5} className="opacity-80 animate-spin" />
+                  ) : 'Submit'}
+            </button>
+          </form>
+        </FormProvider>
+      </div>
+
+      </div>
    </div>
   )
 }
