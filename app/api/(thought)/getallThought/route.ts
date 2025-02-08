ccb2088e8f0816f8da017e17777db5e80cf37216
manish sharma dev <manishvsharma1@gmail.com>
2025-02-07 23:24:20 -1200

    fixed issue in updated state on render

--- app/api/(thought)/getallThought/route.ts
+++ app/api/(thought)/getallThought/route.ts
@@ -1,53 +1,37 @@
 import { thoughtModel } from "@/model/thoughts.model";
 import { ConnectDb } from "@/connections/dbConnect";
-import NodeCache from "node-cache"
+// import NodeCache from "node-cache"
 import { NextResponse } from "next/server";
 
-const myCache = new NodeCache();
+// const myCache = new NodeCache();
 
 export async function GET(request:Request) {
-    await ConnectDb()
-
     try {
 
-        const cacheKey = JSON.stringify("Alldata")
-        const CachedResult = myCache.get(cacheKey)
+        await ConnectDb()
 
-        if(CachedResult){
+        const thoughts = await thoughtModel.find({ }).sort({ createdAt : -1 })
+
+        if(!thoughts){
             return NextResponse.json(
                 {
-                    message : "data already in cache",
-                    data : CachedResult
-                }
-        )
-
-        } else {
-
-            const thoughts = await thoughtModel.find({ }).sort({ createdAt : -1 })
-
-            if(!thoughts){
-                return NextResponse.json(
-                    {
-                        message: "Thoughts fetching Failed"
-                    },
-                    {
-                        status : 500
-                    }
-                )
-            }
-
-            myCache.set(cacheKey,thoughts,3600)
-
-            return NextResponse.json(
-                {
-                    message: "Fetched All Thought",
-                    data  : thoughts
+                    message: "Thoughts fetching Failed"
                 },
                 {
-                    status : 200
+                    status : 500
                 }
-            ) 
-        }   
+            )
+        }
+
+        return NextResponse.json(
+            {
+                message: "Fetched All Thought",
+                data  : thoughts
+            },
+            {
+                status : 200
+            }
+        ) 
         
     } catch (error) {
         return NextResponse.json(
